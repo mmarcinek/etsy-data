ce33300acad3829139129e72e6cc637b2f277e2a
Michael Marcinek <michaelmarcinek@gmail.com>
2015-05-15 12:52:44 -0400

    Saves progress

--- main.js
+++ main.js
@@ -18,3 +18,37 @@ var answer = avg.toFixed(2)
 
 //Drop answer in DOM node:
 document.querySelector('#answer1').textContent = "The average price is $" + answer;
+
+//question 2:
+
+var question2 = [];
+
+var sort = items.filter (function(item){
+  if(item.price > 14 && item.price < 18){
+    question2.push(item.title);
+  }
+});
+
+// console.log(question2);
+
+document.querySelector('#answer2').textContent = question2;
+
+// question 3:
+
+var question3 = [];
+
+var pounds = items.filter (function(item){
+  if(item.currency_code === "GBP") {
+    question3.push(item.title, item.price);
+  }
+});
+
+// console.log(question3);
+
+// document.querySelector('#answer3').textContent = question3;
+
+// var answer3 = document.querySelector('#answer3');
+// answer3.innerText = '';
+// items.forEach( function (item) {
+//   answer3.appendChild(document.createTextNode(item.title + " costs £" + item.price));
+// });
